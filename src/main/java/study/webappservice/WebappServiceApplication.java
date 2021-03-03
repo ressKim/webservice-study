d53a8b75f6e3b19d469faa1f91252b7fc9bdf845
hyukjun kim <sh21dlat@gmail.com>
2021-03-03 23:38:52 +0900

    start webapp service study

---
+++ src/main/java/study/webappservice/WebappServiceApplication.java
@@ -0,0 +1,13 @@
+package study.webappservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebappServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/webappservice/WebappServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package study.webappservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
