44320545ed4d1131aa3cf1fea4869b335dc44b6e
ressKim <sh21dlat@gmail.com>
2021-03-14 20:26:12 +0900

    build.gradle - h2 db add , Post - PostRepository add

--- src/main/java/study/webappservice/web/HelloController.java
+++ src/main/java/study/webappservice/web/HelloController.java
@@ -1,6 +1,5 @@
 package study.webappservice.web;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
---
+++ src/main/java/study/webappservice/web/domain/Test1.java
@@ -0,0 +1,8 @@
+package study.webappservice.web.domain;
+
+import study.webappservice.web.domain.posts.Posts;
+
+public class Test1 {
+
+
+}
