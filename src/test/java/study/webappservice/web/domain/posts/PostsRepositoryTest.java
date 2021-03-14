a492dd1cde90f714a7e0be7984db6ea8bf28ad1d
ressKim <sh21dlat@gmail.com>
2021-03-14 20:35:28 +0900

    PostsRepositoryTest 추가 및 테스트 완료 , p6spy 추가

---
+++ src/test/java/study/webappservice/web/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,43 @@
+package study.webappservice.web.domain.posts;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+
+    @Test
+    public void post_get() {
+
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("abc@abc.com")
+                .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+
+
+    }
+
+
+}
