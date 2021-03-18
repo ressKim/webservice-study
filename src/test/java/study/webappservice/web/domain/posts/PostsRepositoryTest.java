bc678cd66d91789ab350a6e65fa15ce024875700
ressKim <sh21dlat@gmail.com>
2021-03-18 21:29:18 +0900

    Auditing test code add

--- src/test/java/study/webappservice/web/domain/posts/PostsRepositoryTest.java
+++ src/test/java/study/webappservice/web/domain/posts/PostsRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
@@ -35,9 +36,29 @@ class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
 
+    @Test
+    public void Base_TimeEntity_Test(){
+        LocalDateTime now = LocalDateTime.of(2021, 3, 18, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>> posts.getCreatedDate() = " + posts.getCreatedDate() + ", modifiedDate = " + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
 
     }
+
+
 
 
 }
