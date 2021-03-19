e66dadf8629ed87e35319d17fc6539c33db76d41
ressKim <sh21dlat@gmail.com>
2021-03-19 23:20:18 +0900

    전체 조회 화면 구성

--- src/main/java/study/webappservice/service/posts/PostsService.java
+++ src/main/java/study/webappservice/service/posts/PostsService.java
@@ -5,21 +5,27 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import study.webappservice.web.domain.posts.Posts;
 import study.webappservice.web.domain.posts.PostsRepository;
+import study.webappservice.web.dto.PostsListResponseDto;
 import study.webappservice.web.dto.PostsUpdateRequestDto;
 import study.webappservice.web.dto.PostsResponseDto;
 import study.webappservice.web.dto.PostsSaveRequestDto;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 @Service
-@Transactional
+@Transactional(readOnly = true)
 public class PostsService {
 
     private final PostsRepository postsRepository;
 
+    @Transactional(readOnly = false)
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
+    @Transactional(readOnly = false)
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 글이 없습니다. id = " + id));
@@ -36,5 +42,11 @@ public class PostsService {
         return new PostsResponseDto(posts);
     }
 
+
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
 
 }
--- src/main/java/study/webappservice/web/IndexController.java
+++ src/main/java/study/webappservice/web/IndexController.java
@@ -1,13 +1,22 @@
 package study.webappservice.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import study.webappservice.service.posts.PostsService;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+
         return "index";
     }
 
