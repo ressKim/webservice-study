36425ea90eebacea9cc22cfd2139655a31f61fff
ressKim <sh21dlat@gmail.com>
2021-04-03 20:40:54 +0900

    static index.js 경로 잘못설정된 것 수정

--- src/test/java/study/webappservice/web/dto/HelloResponseDtoTest.java
+++ src/test/java/study/webappservice/web/dto/HelloResponseDtoTest.java
@@ -2,9 +2,6 @@ package study.webappservice.web.dto;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 
 //@SpringBootTest
@@ -13,13 +10,13 @@ class HelloResponseDtoTest {
     @Test
     public void lombok_test(){
 
-        String name = "test";
+        String name = "static/test";
         int amount = 1000;
 
         HelloResponseDto dto = new HelloResponseDto(name, amount);
 
         Assertions.assertThat(dto.getAmount()).isEqualTo(1000);
-        Assertions.assertThat(dto.getName()).isEqualTo("test");
+        Assertions.assertThat(dto.getName()).isEqualTo("static/test");
     }
 
 }
